79a1366a2161bf9969e998e0c4efb5dd8bcbabe2
Aurélie VIOLETTE <orwel.violette@gmail.com>
2023-06-11 00:46:10 +0200

    Add tailwind config

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header></header>
+  <header class="w-full pt-4 pb-3 pl-6 bg-blue">
+    <span class="text-4xl font-hand text-cream">Atelier 44</span>
+  </header>
   <RouterView />
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        hand: ['Arizonia', 'cursive']
+      },
+      colors: {
+        'blue-gray': {
+          DEFAULT: 'hsl(209, 21%, 45%)'
+        },
+        blue: {
+          DEFAULT: 'hsl(219, 48%, 15%)'
+        },
+        copper: {
+          DEFAULT: 'hsl(27, 40%, 52%)'
+        },
+        brown: {
+          DEFAULT: 'hsl(24, 46%, 37%)'
+        },
+        'light-pink': {
+          DEFAULT: 'hsl(60, 2%, 88%)'
+        },
+        cream: {
+          DEFAULT: 'hsl(60, 3%, 93%)'
+        }
+      }
+    }
+  },
+  plugins: []
+}
